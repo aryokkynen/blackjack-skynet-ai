b34f368062d520a6a89f72fcee2f6bf40668292c
Ryökkynen Antti <a1503069@myy.haaga-helia.fi>
2017-09-15 12:19:18 +0300

    Improved gaming logic, added few changeable variables

--- src/main/java/ai_blackjack/skynet/App.java
+++ src/main/java/ai_blackjack/skynet/App.java
@@ -3,21 +3,40 @@ package ai_blackjack.skynet;
 import java.text.DecimalFormat;
 
 public class App {
-	
+
 	public static void main(String[] args) {
+
+		/*
+		 * Rules: Decks in shoe = 4 (This can be changed from Dealer.java)
+		 * Player draws first, then dealer for starting hand, repeat two times. 
+		 * Create random game tolerance for player (Starting sum(Set to 12) + random(1-5)),
+		 * this is sum which player should not go over unless losing to dealer. 
+		 * This can be changed by modifying variable min/max tolerance
+		 * 
+		 * Decks are shuffled after each game.
+		 * 
+		 * Check for bust -> player draws -> Check for blackjack for player ->
+		 * check for dealer bust -> dealer draws -> check for blackjack for
+		 * dealer -> Repeat.
+		 */
+		
+		// CHANGEABLE VARIABLES
+		int min_tolerance = 1; 
+		int max_tolerance = 5;
+		int games = 500000;
 
 		DecimalFormat df = new DecimalFormat("####0.00");
 		long start = System.currentTimeMillis();
-		
+
 		int dealer_wins = 0;
 		int player_wins = 0;
 
-		int games = 5000;
 		Dealer d = new Dealer();
 		d.Make_deck();
-		
+
 		for (int i = 0; i < games; i++) {
-			
+
+			System.out.println("*** SIMULATING GAME " + (i + 1) + " ***");
 			int player_hand;
 			int dealer_hand;
 			int player_card1 = d.Generate_random_card();
@@ -25,14 +44,14 @@ public class App {
 			int player_card2 = d.Generate_random_card();
 			int dealer_card2 = d.Generate_random_card();
 			int player_starting_sum = 12;
-			
+
 			int player_card_count = 2;
-			int dealer_card_count = 2;			
-			
+			int dealer_card_count = 2;
+
 			boolean player_win = false;
 			boolean player_stay = false;
 			boolean player_bust = false;
-			
+
 			boolean dealer_win = false;
 			boolean dealer_stay = false;
 			boolean dealer_bust = false;
@@ -40,46 +59,23 @@ public class App {
 			player_hand = player_card1 + player_card2;
 			dealer_hand = dealer_card1 + dealer_card2;
 			int player_starting_hand = player_hand;
-			
-			System.out.println("Player starting hand, player card{1} " + player_card1 + ", player card{2} " + player_card2 + " total: " + player_hand);
-			System.out.println("Dealer starting hand, dealer card{1} " + dealer_card1 + " , dealer card{2} " + dealer_card2 + " total: " + dealer_hand);
-			int tolerance = player_starting_sum  + (1 + (int) (Math.random() * 5));
-			
-			dealer_win = d.check_for_blackjack(dealer_hand);
 
+			System.out.println("Player starting hand, player card{1} " + player_card1 + ", player card{2} "
+					+ player_card2 + " total: " + player_hand);
+			System.out.println("Dealer starting hand, dealer card{1} " + dealer_card1 + " , dealer card{2} "
+					+ dealer_card2 + " total: " + dealer_hand);
+			int tolerance = player_starting_sum + (min_tolerance + (int) (Math.random() * max_tolerance));
+
+			dealer_win = d.check_for_blackjack(dealer_hand);
+			System.out.println("Random (player) tolerance for round " + (i + 1 + " is ") + tolerance);
 			// Do stuff
 			do {
-				if (dealer_hand < 17) {
-					int dealer_draw = d.Generate_random_card();
-					dealer_hand = dealer_hand + dealer_draw;
-					if (dealer_hand == 1) {
-						if (dealer_hand <= 12) {
-							dealer_hand = 11;
-						}
-					}
-					dealer_win = d.check_for_blackjack(dealer_hand);
-					System.out.println("Dealer draw! " + dealer_draw);
-					dealer_card_count++;
-					if (dealer_win) {
-						System.out.println("Dealer blackjack!");
-						d.Check_shuffle();
-						break;
-					}
-				}
-				
-				// check for dealer bust before continuing
-				dealer_bust = d.check_hand(dealer_hand);
-				if (!dealer_bust){
-					System.out.println("Dealer bust! Over 21. Dealer hand: " + dealer_hand );
-					d.Check_shuffle();
-					break;
-				}
-				
-				if (player_hand < tolerance || player_starting_hand > tolerance || player_hand < dealer_hand){
+
+				if (player_hand < tolerance || player_starting_hand > tolerance || player_hand < dealer_hand) {
 					int player_draw = d.Generate_random_card();
 					player_hand = player_hand + player_draw;
-					
-					//Determine if Ace should be used as "11"
+
+					// Determine if Ace should be used as "11"
 					if (player_draw == 1) {
 						if (player_hand <= 12) {
 							player_draw = 11;
@@ -94,29 +90,61 @@ public class App {
 						break;
 					}
 				}
-				
+
 				// check for dealer bust before continuing
 				player_bust = d.check_hand(player_hand);
-				if (!player_bust){
-					System.out.println("Player bust! Over 21. Player hand: " + player_hand );
+				if (!player_bust) {
+					System.out.println("Player bust! Over 21. Player hand: " + player_hand);
 					d.Check_shuffle();
 					break;
 				}
-				
+
+				// check for dealer bust before continuing
+				dealer_bust = d.check_hand(dealer_hand);
+				if (!dealer_bust) {
+					System.out.println("Dealer bust! Over 21. Dealer hand: " + dealer_hand);
+					d.Check_shuffle();
+					break;
+				}
+
+				if (dealer_hand < 17) {
+					int dealer_draw = d.Generate_random_card();
+					dealer_hand = dealer_hand + dealer_draw;
+					// Determine if Ace should be used as "11"
+					if (dealer_hand == 1) {
+						if (dealer_hand <= 12) {
+							dealer_hand = 11;
+						}
+					}
+					dealer_win = d.check_for_blackjack(dealer_hand);
+					System.out.println("Dealer draw! " + dealer_draw);
+					dealer_card_count++;
+					if (dealer_win) {
+						System.out.println("Dealer blackjack!");
+						d.Check_shuffle();
+						break;
+					}
+				}
+
 				if (player_hand >= tolerance && dealer_hand >= 16) {
 					d.Check_shuffle();
 					break;
 				}
-						
-			} while (true); 
-					
-			System.out.println("Random (player) tolerance: " + tolerance);
+
+			} while (true);
+
+			// Check if your hand value is equal or less than dealer, draw more.
+			if (player_hand <= dealer_hand && player_hand < 21 && dealer_hand < 21) {
+				player_hand = player_hand + d.Generate_random_card();
+				player_card_count++;
+			}
+
 			System.out.println("Player hand: " + player_hand);
-			System.out.println("Dealer hand: " + dealer_hand);			
-				
-			if (d.check_hand(player_hand) && !dealer_win) {		
-								
-				if (player_hand > dealer_hand){
+			System.out.println("Dealer hand: " + dealer_hand);
+
+			if (d.check_hand(player_hand) && !dealer_win) {
+
+				if (player_hand > dealer_hand) {
 					player_win = true;
 					player_wins++;
 					System.out.println("Player won: PH " + player_hand + " VS DH " + dealer_hand);
@@ -125,7 +153,7 @@ public class App {
 					dealer_wins++;
 					System.out.println("Player lost: PH " + player_hand + " VS DH " + dealer_hand);
 				}
-				
+
 			} else {
 				player_win = false;
 				dealer_wins++;
@@ -135,29 +163,28 @@ public class App {
 			System.out.println("Player cardcount: " + player_card_count);
 			System.out.println("Dealer cardcount: " + dealer_card_count + "\n");
 
-		//End FOR
+			// End FOR
 		}
-			
+
 		long end = System.currentTimeMillis();
 		System.out.println("****************************************");
 		System.out.println("Player wins: " + player_wins);
 		System.out.println("Dealer wins: " + dealer_wins);
 		System.out.println("Total games: " + games);
-		System.out.println("Player win " + df.format(100/((double)games / (double) player_wins)) + "%");
+		System.out.println("Player win " + df.format(100 / ((double) games / (double) player_wins)) + "%");
 		System.out.println("Execution time is " + df.format((end - start) / 1000d) + " seconds");
 		System.out.println("****************************************");
-		
+
 	}
-	
-	public static boolean flip_coin(){
-		int flip = 1 + (int) (Math.random() * 2);		
-		
+
+	public static boolean flip_coin() {
+		int flip = 1 + (int) (Math.random() * 2);
+
 		if (flip == 1) {
 			return true;
-		} 
-		else {
+		} else {
 			return false;
 		}
 	}
-	
+
 }
